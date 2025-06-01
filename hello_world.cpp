48ece0a9f42e29507ca916a0090bd7c9049a234f
ka-techkaa <Kate.cherkasina@gmail.com>
2025-06-01 17:59:59 +0000

    change comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-<<<<<<< HEAD
-#include <string>//нач
-//комент
+#include <string>
 int main() {
     std::string name;
     std::cout << "Enter your name: ";
@@ -10,17 +8,3 @@ int main() {
     return 0; 
 }
 //кон
-=======
-#include <string> //start
-// comment
-int
-main()
-{
-  std::string name;
-  std::cout << "Enter your name: ";
-  std::cin >> name;
-  std::cout << "Hello world from " << name << std::endl;
-  return 0;
-}
-// end
->>>>>>> 76208f6 (Apply clang-format Mozilla style)
