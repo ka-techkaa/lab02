6346abcb6b0440c302a284aa027b3aa175eb14e6
ka-techkaa <Kate.cherkasina@gmail.com>
2025-06-01 17:54:24 +0000

    Apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 #include <string>//нач
 //комент
 int main() {
@@ -9,3 +10,17 @@ int main() {
     return 0; 
 }
 //кон
+=======
+#include <string> //start
+// comment
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
+}
+// end
+>>>>>>> 76208f6 (Apply clang-format Mozilla style)
